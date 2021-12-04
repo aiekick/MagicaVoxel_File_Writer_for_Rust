b3b300c22109d5a53fd0f00534785be3452a1a16
aiekick <aiekick@funparadigm.com>
2021-12-04 04:00:02 +0100

    -

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,21 @@
-mod vox_writer;
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+pub fn get_mvid(a : char, b : char, c : char, d : char) -> u32
+{
+    return ((a as i32) | ((b as i32) << 8) | ((c as i32) << 16) | ((d as i32) << 24)) as u32;
+}
+
+#[test]
+fn test_get_mvid()
+{
+    assert_eq!(get_mvid('V', 'O',  'X', ' '), 542658390);
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
 
 fn main()
 {
-    let  id = vox_writer::get_mvid('V', 'O',  'X', ' ');
+    let  id = get_mvid('V', 'O',  'X', ' ');
 
     println!("id is {}", id);
 
