50e7b5e17f51b7e4f91ebe1fa99949617fb172af
aiekick <aiekick@funparadigm.com>
2022-02-28 20:04:01 +0100

    -

--- src/main.rs
+++ src/main.rs
@@ -13,9 +13,6 @@ fn step(edge:f64, v:f64)->f64 { if v < edge { return 0.0; } return 1.0; }
 #[allow(dead_code)]
 fn sign(v:f64) -> f64 { if v < 0.0 { return -1.0; } return 1.0; }
 
-#[allow(dead_code)]
-fn atan2(x:f64, y:f64)->f64 { if x != 0.0 {return y / x; } return 1.0; }
-
 /*
 struct vec2
 {
@@ -50,51 +47,48 @@ fn generate_julia_revolute()
     {
         let mut vox = VoxWriter::create_empty();
 
-        const SX:i32 = 300;
-        const SY:i32 = 300;
-        const SZ:i32 = 300;
-        const ZOOM:f64 = 5.0;
+        const SIZE:i32 = 300;
+        const ZOOM_XZ:f64 = 5.0;
+        const ZOOM_Y:f64 = 5.0;
 
-        for i in 0..SX {
-            for j in 0..SY {
-                for k in 0..SZ {
-                    let px = (i as f64 * 2.0 / SX as f64 - 1.0) * ZOOM;
-                    let py = (j as f64 * 2.0 / SY as f64 - 1.0) * ZOOM;
-                    let pz = (k as f64 * 2.0 / SZ as f64 - 1.0) * ZOOM;
+        for i in 0..SIZE {
+            for j in 0..SIZE {
+                for k in 0..SIZE {
+                    let px = (i as f64 * 2.0 / SIZE as f64 - 1.0) * ZOOM_XZ;
+                    let py = (j as f64 * 2.0 / SIZE as f64 - 1.0) * ZOOM_Y;
+                    let pz = (k as f64 * 2.0 / SIZE as f64 - 1.0) * ZOOM_XZ;
 
-                    let an = atan2(px, py);
+                    let an = f64::atan2(px, pz);
 
                     let cx = mix(0.2, -0.5, f64::sin(an * 2.0));
                     let cy = mix(0.5, 0.0, f64::sin(an * 3.0));
 
-                    let path = f64::sqrt(px * px + py * py) - 3.0;
+                    let path = f64::sqrt(px * px + pz * pz) - 3.0;
 
                     let mut rev_x = path;
-                    let mut rev_z = pz;
+                    let mut rev_y = py;
 
-                    // rev *= mat2(cos(a), -sin(a), sin(a), cos(a));
-                    // p.x * self.m[0] + p.x * self.m[1],
-                    // p.y * self.m[2] + p.y * self.m[3]
-                    
-                    let _ca = an.cos();
-                    let _sa = an.sin();
-                    rev_x *= _ca - _sa;
-                    rev_y *= _sa + _ca;
-                    
+                    let _ca = f64::cos(an);
+                    let _sa = f64::sin(an);
+
+                    //let tmp_x = rev_x;
+                    //rev_x = rev_x * _ca - rev_y - _sa;
+                    //rev_y = tmp_x * _sa - rev_y + _sa;
+
                     let mut kk = 1.0;
                     let mut hh = 1.0;
 
                     for _idx in 0..5
                     {
                         hh *= 4.0 * kk;
-                        kk = rev_x * rev_x + rev_z * rev_z;
+                        kk = rev_x * rev_x + rev_y * rev_y;
                         if kk > 4.
                         {
                             break;
                         }
-                        let tmp_x = rev_x;
-                        rev_x = rev_x * rev_x - rev_z * rev_z + cx;
-                        rev_z = 2.0 * tmp_x * rev_z + cy;
+                       let tmp_x = rev_x;
+                        rev_x = rev_x * rev_x - rev_y * rev_y + cx;
+                        rev_y = 2.0 * tmp_x * rev_y + cy;
                     }
 
                     let df = f64::sqrt(kk / hh) * f64::log10(kk);
@@ -102,7 +96,8 @@ fn generate_julia_revolute()
                     if f64::abs(df) - 0.01 < 0.0
                     {
                         let cube_color = (i + j + k) % 255 + 1;
-                        vox.add_voxel(i, j, k, cube_color);
+                        //vox.add_voxel(i, j, k, cube_color);
+                        vox.add_voxel(i, k, j, cube_color); // magicavoxel use the z as up axis
                     }
                 }
             }
